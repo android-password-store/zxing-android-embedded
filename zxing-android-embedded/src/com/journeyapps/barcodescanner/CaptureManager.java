835b877160c5fa42b1e174f5256ac24ab952b6f6
Harsh Shandilya <me@msfjarvis.dev>
2020-06-29 23:36:21 +0530

    zxing-android-embedded: Use AppCompat theming
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- zxing-android-embedded/src/com/journeyapps/barcodescanner/CaptureManager.java
+++ zxing-android-embedded/src/com/journeyapps/barcodescanner/CaptureManager.java
@@ -3,7 +3,6 @@ package com.journeyapps.barcodescanner;
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
@@ -18,6 +17,7 @@ import android.view.Surface;
 import android.view.Window;
 import android.view.WindowManager;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
